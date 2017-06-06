a2b764ea25c18d20b1d0c8721124f3c94d7fa95b
Mark Meretzky <mark.meretzky@nyu.edu>
2017-06-06 16:29:05 -0400

    Create flag.py

---
+++ flag.py
@@ -0,0 +1,20 @@
+"""
+helloworld.py
+
+Write a flag to the standard output.
+Then pass exit status 0 back to the operating system to indicate success.
+"""
+
+import sys
+
+print("* * * * * * =====================")
+print(" * * * * *  =====================")
+print("* * * * * * =====================")
+print(" * * * * *  =====================")
+print("* * * * * * =====================")
+print("=================================")
+print("=================================")
+print("=========E pluribus unum=========")
+print("=================================")
+
+sys.exit(0)
