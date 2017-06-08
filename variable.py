465192ef9a4be96f66131b08380882bcb93474fc
Mark Meretzky <mark.meretzky@gmail.com>
2017-06-08 07:08:26 -0400

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,15 @@
+"""
+variable.py
+
+Create three variables using three assignment statements.
+Output the value of each variable.
+"""
+
+import sys
+
+i = 10
+j = 20
+sum = i + j
+print("The sum of ", i, " and ", j, " is ", sum, ".", sep = "")
+
+sys.exit(0)
