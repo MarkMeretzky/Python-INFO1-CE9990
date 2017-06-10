3a910f902645094441eb81ede3419ef32db2b454
Mark Meretzky <mark.meretzky@gmail.com>
2017-06-09 22:45:19 -0400

    Create nottoprogram1.py

---
+++ nottoprogram1.py
@@ -0,0 +1,20 @@
+"""
+nottoprogram1.py
+
+Output the integers from 0 to 9 inclusive, one per line. 
+"""
+
+import sys
+
+print(0)
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+
+sys.exit(0)
