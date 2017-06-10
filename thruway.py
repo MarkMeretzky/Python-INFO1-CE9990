65ebae939ab907596be492614c78576523c0cb21
Mark Meretzky <mark.meretzky@gmail.com>
2017-06-10 07:19:59 -0400

    Create thruway.py

---
+++ thruway.py
@@ -0,0 +1,26 @@
+"""
+thruway.py
+Print the signs you see as you drive north on the New York State Thruway. 
+"""
+
+import sys
+
+#This format string consists of five lines of text.
+#The fifth line is empty (i.e., consists of one newline character).
+
+f = """\
++--------------+
+| Albany   {:3} |
+| Montreal {:3} |
+| Buffalo  {:3} |
++--------------+
+\
+"""
+
+a = 98   #miles to Albany
+
+while a >= 38:
+    print(f.format(a, a + 300, a + 360))
+    a = a - 10   #or a -= 10
+
+sys.exit(0)
