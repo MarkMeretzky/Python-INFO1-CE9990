5479dc0a56e353deacd8630790a0fe22b19bd91f
Mark Meretzky <mark.meretzky@gmail.com>
2017-06-11 20:55:24 -0400

    Create nestedloops.py

---
+++ nestedloops.py
@@ -0,0 +1,21 @@
+"""
+nestedloops.py
+
+Output the chorus of "Lucy in the Sky with Wiamonds".
+"""
+
+import sys
+
+outer = 1
+while outer <= 3:
+
+    inner = 1
+    while inner <= 3:
+        print("Lucy in the sky with diammonds")
+        inner += 1
+
+    print("Aaaaaaaaaaaaaaaaaaaaaaaaaaaaah")
+    print()
+    outer += 1
+
+sys.exit(0)
