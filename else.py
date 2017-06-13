91de51376899e9346e14f7670021a188380465d4
Mark Meretzky <mark.meretzky@gmail.com>
2017-06-12 21:43:42 -0400

    Create else.py

---
+++ else.py
@@ -0,0 +1,24 @@
+"""
+else.py
+
+Demonstrate the if/else statement.
+"""
+
+import sys
+
+trump = input("How many electoral votes did Trump get? ")
+trump = int(trump)
+
+clinton = input("How many electoral votes did Clinton get? ")
+clinton = int(clinton)
+
+print()   #Skip a line.
+
+if trump == clinton:
+    print("They're tied.  Please inform the House of Representatives")
+    print("that the Electoral College is hung.")
+else:
+    print("There's a clear winner.")
+    print("Thank you, electorate.")
+
+sys.exit(0)
