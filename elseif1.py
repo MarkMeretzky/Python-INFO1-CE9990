953c6d6be452ba1b50763ec1ac1e518b4b8b67a1
Mark Meretzky <mark.meretzky@gmail.com>
2017-06-12 22:12:22 -0400

    Create elseif1.py

---
+++ elseif1.py
@@ -0,0 +1,25 @@
+"""
+elseif1.py
+
+Do we always need to perform both comparisons?
+"""
+
+import sys
+
+receipts = input("How much were our receipts? ")
+receipts = int(receipts)
+
+expenditures = input("How much were our expenditures? ")
+expenditures = int(expenditures)
+
+print()   #Skip a line.
+
+if receipts > expenditures:
+    print("We're making money.")
+else:
+    print("We're not making any money.")
+    
+if receipts < expenditures:
+    print("In fact, we're losing money.")
+
+sys.exit(0)
