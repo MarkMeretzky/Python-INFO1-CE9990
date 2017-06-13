d8a659ecce3c613400081e916aeedeefac3f942f
Mark Meretzky <mark.meretzky@gmail.com>
2017-06-13 13:42:14 -0400

    Create fontsize.py

---
+++ fontsize.py
@@ -0,0 +1,27 @@
+"""
+fontsize.py
+
+Output an HTML stylesheet showing different font sizes.
+"""
+
+import sys
+
+print("<HTML>")
+print("<BODY>")
+print("<H1>Stylesheet</H1>")
+print()
+
+points = 1
+
+while points < 13:
+    print("<P STYLE = \"font-size: ", points, "pt\">", sep = "")
+    print(points, "point")
+    print("<BR>")
+    print("Pack my box with five dozen liquor jugs.")
+    print("</P>")
+    print()
+    points += 1
+
+print("</BODY>")
+print("</HTML>")
+sys.exit(0)
