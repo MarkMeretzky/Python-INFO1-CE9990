db069cbf291f781785fbae2b4d308bcf4deed008
Mark Meretzky <mark.meretzky@gmail.com>
2017-06-12 21:28:00 -0400

    Create if.py

---
+++ if.py
@@ -0,0 +1,25 @@
+"""
+if.C
+
+Demonstrate the if statement.
+"""
+
+import sys
+
+trump = input("How many electoral votes did Trump get? ")
+trump = int(trump)
+
+clinton = input("How many electoral votes did Clinton get? ")
+clinton = int(clinton)
+
+print()   #Skip a line.
+
+if trump == clinton:
+    print("They're tied.  Please inform the House of Representatives")
+    print("that the Electoral College is hung.")
+
+if trump != clinton:
+    print("There's a clear winner.")
+    print("Thank you, electorate.")
+
+sys.exit(0)
