a27436721c250eee14ba44b192216a2ed79c402f
Mark Meretzky <mark.meretzky@gmail.com>
2017-06-18 10:06:05 -0400

    Create list3.py

---
+++ list3.py
@@ -0,0 +1,27 @@
+"""
+list3.py
+
+Demonstrate a list of strings.
+"""
+
+import sys
+
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+for month in months:
+    print(month)
+
+sys.exit(0)
