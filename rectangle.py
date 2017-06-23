ed3bb1516a6aeacc34b422326587f92aacdedfd2
Mark Meretzky <mark.meretzky@gmail.com>
2017-06-23 07:50:59 -0400

    Create rectangle.py

---
+++ rectangle.py
@@ -0,0 +1,20 @@
+"""
+rectangle.py
+
+Output a rectangle of X's.
+"""
+
+import sys
+
+outer = 1
+while outer <= 5:
+    
+    inner = 1
+    while inner <= 36:
+        print("X", end = "")
+        inner += 1
+
+    print()   #Output one newline character.
+    outer += 1
+
+sys.exit(0)
