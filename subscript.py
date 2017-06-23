fb42d64a0e7a6e8f133ee6f0f97943d5f6881457
Mark Meretzky <mark.meretzky@gmail.com>
2017-06-23 18:21:32 -0400

    Create subscript.py

---
+++ subscript.py
@@ -0,0 +1,27 @@
+"""
+subscript.py
+
+Demonstrate string subscripting (i.e., indexing).
+"""
+
+import sys
+
+s = "counterclockwise"
+
+#Don't bother to print the period at the end of each sentence.
+print("The value of s is", s)
+print("The type of s is", type(s))
+print("The length of s is", len(s))
+print()
+
+print("The first character is", s[0])
+print("The second character is", s[1])
+print("The third character is", s[2])
+print("The sixteenth character is", s[15])
+print()
+
+print("The last character is", s[-1])
+print("The next-to-last character is", s[-2])
+print("The third-from-last character is", s[-3])
+
+sys.exit(0)
