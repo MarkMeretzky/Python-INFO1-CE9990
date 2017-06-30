31c0c9db5e3b47cb16a2321e0923f63a4fb38bfd
Mark Meretzky <mark.meretzky@gmail.com>
2017-06-30 07:24:28 -0400

    Create for.py

---
+++ for.py
@@ -0,0 +1,20 @@
+"""
+for.py
+
+Output the integers from 0 to 9 inclusive, one per line,
+using a while loop and a for loop.
+"""
+
+import sys
+
+i = 0
+while i < 10:
+    print(i)
+    i += 1
+
+print()   #Skip a line.
+
+for i in range(0, 10, 1):   #range(0, 10) or range(10) would do the same thing
+    print(i)
+
+sys.exit(0)
