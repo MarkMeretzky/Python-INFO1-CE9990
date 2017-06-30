334db1856923ed74911f199a5ca34c7a264a2c5e
Mark Meretzky <mark.meretzky@gmail.com>
2017-06-30 15:51:03 -0400

    Create toroman.py

---
+++ toroman.py
@@ -0,0 +1,58 @@
+"""
+toroman1.py
+
+Convert an integer to Roman numerals.
+"""
+
+import sys
+
+def getInt(prompt):
+    "Let the user input one integer."
+    assert isinstance(prompt, str)
+
+    try:
+        s = input(prompt + " ")
+    except EOFError:
+        sys.exit(0)
+
+    try:
+        i = int(s)
+    except ValueError:
+        print("Sorry, \"", s, "\" is not an integer.", sep = "")
+        sys.exit(1)
+
+    return i
+
+
+ones      = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+tens      = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+hundreds  = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+thousands = ["", "M", "MM", "MMM", "MMMM"]
+
+def toRoman(n):
+    "Convert one integer to a Roman numeral."
+    assert isinstance(n, int) and 1 <= n and n <= 4999
+
+    s = thousands[n // 1000]
+    i %= 1000                #Chop off the thousands place; means n = n % 1000
+
+    s += hundreds[n // 100]  #means s = s + hundreds[n // 100]
+    n %= 100                 #Chop off the hundreds place; means n = n % 100
+
+    s += tens[n // 10]       #means s = s + tens[n // 10]
+    n %= 10                  #Chop off the tens place; means n = n % 10
+
+    s += ones[n]             #means s = s + ones[n]
+    return s
+
+
+while True:
+    while True:
+        n = getInt("Please type an integer in the range 1 to 4999 inclusive:")
+        if 1 <= n and n <= 4999:
+            break
+        print("Sorry,", n, "is out of range.")
+
+    romanNumeral = toRoman(n)
+    print(n, "=", romanNumeral)
+    print()
