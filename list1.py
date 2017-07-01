63431d01ad6c197487e612b5ff88e5130528dc35
Mark Meretzky <mark.meretzky@gmail.com>
2017-07-01 18:15:37 -0400

    Create list1.py

---
+++ list1.py
@@ -0,0 +1,32 @@
+"""
+list1.py
+
+Demonstrate how they programmed before they invented lists.
+"""
+
+import sys
+
+january = 31
+february = 28
+march = 31
+april = 30
+may = 31
+june = 30
+july = 31
+august = 31
+september = 30
+october = 31
+november = 30
+december = 31
+
+n = 12   #have to count them yourself
+
+print("A year has", n, "months.")
+print("The first month has", january, "days.")
+print("The second month has", february, "days.")
+
+print("The third-to-last month month has", october, "days.")
+print("The next-to-last month month has", november, "days.")
+print("The last month month has", december, "days.")
+
+sys.exit(0)
