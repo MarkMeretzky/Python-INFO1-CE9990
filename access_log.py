8af7882dc23c6c0d8066a8511a052671e688adeb
Mark Meretzky <mark.meretzky@gmail.com>
2017-07-05 23:07:16 -0400

    Create access_log.py

---
+++ access_log.py
@@ -0,0 +1,27 @@
+"""
+access_log.py
+
+Print the lines in the Apache web server's access_log file that mention the
+Python course.
+"""
+
+import sys
+
+#Fedora Linux
+filename = "/var/log/httpd/access_log-20170612"
+
+try:
+    lines = open(filename)
+except FileNotFoundError:
+    print("Sorry, could not find file \"", filename, "\".", sep = "")
+    sys.exit(1)
+except PermissionError:
+    print("Sorry, no permission to open file \"", filename, "\".", sep = "")
+    sys.exit(1)
+
+for line in lines:
+    if "/~meretzkm/python/" in line:
+        print(line, end = "")
+
+lines.close()
+sys.exit(0)
