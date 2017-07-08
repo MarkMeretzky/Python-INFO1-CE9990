c6b4c2a537dbc14de9192f6500b6e2afbc78d74f
Mark Meretzky <mark.meretzky@gmail.com>
2017-07-08 16:15:06 -0400

    Create restaurant.py

---
+++ restaurant.py
@@ -0,0 +1,30 @@
+"""
+restaurant.py
+
+Output the inspection results for Wo Hop, 17 Mott Street, NY, NY 10013
+"""
+
+import sys
+import csv   #Comma-separated values.  Do not name this Python script csv.py.
+
+filename = "DOHMH_New_York_City_Restaurant_Inspection_Results.csv"
+
+try:
+    csvfile = open(filename, encoding = "utf-8", newline = "")
+except FileNotFoundError:
+    print("Sorry, could not find file \"", filename, "\".", sep = "")
+    sys.exit(1)
+except PermissionError:
+    print("Sorry, no permission to open file \"", filename, "\".", sep = "")
+    sys.exit(1)
+
+lines = csv.reader(csvfile)
+
+for line in lines:              #During each iteration, line is a list of strings.
+    if line[0] == "41320866":   #CAMIS number for Wo Hop 17
+        print(line[1], line[8]) #name and inspection date
+        print(line[11])         #violation description
+        print()
+
+csvfile.close()
+sys.exit(0)
