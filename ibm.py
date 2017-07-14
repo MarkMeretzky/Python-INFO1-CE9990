6f27871273bab0446dbf44ec7144ebe726feb9ff
Mark Meretzky <mark.meretzky@gmail.com>
2017-07-14 07:27:16 -0400

    Create ibm.py

---
+++ ibm.py
@@ -0,0 +1,41 @@
+"""
+ibm.py
+
+Produce exit status 0 if the last price of IBM was >= 154,
+exit status 1 if it was < 154.
+Produce exit status 2 if we could not get the price.
+
+To avoid the SSL: CERTIFICATE_VERIFY_FAILED error on macOS, run
+Applications -> Python 3.6 -> Install Certificates.command
+"""
+
+import sys
+import urllib.request
+
+#For the l1 code, see http://www.jarloo.com/yahoo_finance/
+url = "http://finance.yahoo.com/d/quotes.csv?s=IBM&f=l1"
+
+try:
+    u = urllib.request.urlopen(url)
+except urllib.error.URLError:
+    sys.exit(2)
+
+line = u.readline()      #line is a sequence of bytes
+u.close()
+
+try:
+    s = line.decode()    #s is a string of characters
+except urllib.error.URLError as error:
+    print("urllib.error.URLError", error)
+    sys.exit(1)
+
+try:
+    price = float(s)
+except ValueError:
+    sys.exit(2)
+
+#print(price)
+if price >= 154:
+    sys.exit(0)   #success
+else:
+    sys.exit(1)   #failure
