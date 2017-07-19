b8cea623c879af6a613b5b06131e460c6eff30a6
Mark Meretzky <mark.meretzky@gmail.com>
2017-07-18 21:47:16 -0400

    Create do1.py

---
+++ do1.py
@@ -0,0 +1,35 @@
+"""
+do1.py
+
+Demonstrate how they programmed before they invented dictionaries.
+notes is a list of seven lists.
+"""
+
+import sys
+
+notes = [
+    ["do", "deer, a female deer"],
+    ["re", "drop of golden sun"],
+    ["me", "name I call myself"],
+    ["fa", "long, long way to run"],
+    ["so", "needle pulling thread"],
+    ["la", "note to follow so"],
+    ["ti", "drink with jam and bread"]
+]
+
+while True:
+    try:
+        n = input("Please type a note (e.g., do): ")
+    except EOFError:
+        sys.exit(0)
+
+    for note in notes:   #note is a list of two strings
+        if note[0] == n:
+            print(n.capitalize(), ", a ", note[1], ".", sep = "")
+            break        #Break out of the for loop, skipping the else.
+    else:
+        #Arrive here if the break was not executed,
+        #i.e., if the for loop did not find n.
+        print("Sorry, \"", n, "\" is not a note.", sep = "")
+        
+    print()
