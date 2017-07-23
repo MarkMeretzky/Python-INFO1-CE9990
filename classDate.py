f2e479faa76a41d361d56390e48776066f9cb094
Mark Meretzky <mark.meretzky@gmail.com>
2017-07-23 08:12:07 -0400

    Create classDate.py

---
+++ classDate.py
@@ -0,0 +1,103 @@
+"""
+classDate.py
+
+Create a class named Date and call one of its class methods (monthsInYear).
+Then create an object named d of class Date and call its methods.
+"""
+
+import sys
+
+class Date(object):
+    lengths = [
+        None,
+        31, #January
+        28, #February
+        31, #March
+        30, #April
+        31, #May
+        30, #June
+        31, #July
+        31, #August
+        30, #September
+        31, #October
+        30, #November
+        31  #December
+    ]
+
+    def __init__(self, month, day, year):
+        assert isinstance(year, int)
+        assert isinstance(month, int) and 1 <= month and month <= 12
+        assert isinstance(day, int) and 1 <= day and day <= Date.lengths[month]
+        self.year = year
+        self.month = month
+        self.day = day
+
+    #These three methods are getters.
+
+    def getYear(self):
+        "Return my year."
+        return self.year
+
+    def getMonth(self):
+        "Return the number of my month (1 to 12 inclusive)."
+        return self.month
+
+    def getDay(self):
+        "Return the number of my day (1 to the length of my month, inclusive)."
+        return self.day
+
+    def __str__(self):
+        "Return a string that looks like the contents of myself."
+        return "{:02}/{:02}/{:04}".format(self.month, self.day, self.year)
+
+    def dayOfYear(self):
+        "Return my day of the year: a number in the range 1 to 365 inclusive."
+        return sum(Date.lengths[1:self.month]) + self.day
+
+    def nextDay(self):
+        "Move myself one day into the future."
+        if self.day < Date.lengths[self.month]:
+            self.day += 1
+        else:
+            self.day = 1         #Go to the first day of the next month.
+            if self.month < len(Date.lengths) - 1:
+                self.month += 1
+            else:
+                self.month = 1   #Go to the first month of the next year.
+                self.year += 1
+
+    def nextDays(self, n):
+        "Move myself n days into the future."
+        assert isinstance(n, int) and n >= 0
+        for i in range(n):
+            self.nextDay()       #Call the method in line 56.
+
+    def monthsInYear():
+        "Return the number of months in a year.  This function is selfless.
+        return len(Date.lengths) - 1;
+
+    #The definition of class Date ends here.
+
+
+print("months in year =", Date.monthsInYear())
+d = Date(12, 31, 2017)         #Call the method in line 27.
+print("type(d) =", type(d))
+print()
+
+#These three statements do the same thing:
+print("d =", d)
+print("d =", str(d))
+print("d =", d.__str__())      #Call the method in line 47.
+print()
+
+print("month =", d.getMonth()) #Call the method in line 39.
+print("day =", d.getDay())     #Call the method in line 42.
+print("year =", d.getYear())   #Call the method in line 36.
+print()
+
+print("{} is day number {} of the year {}.".format(d, d.dayOfYear(), d.getYear()))
+d.nextDay()                    #Call the method in line 56.
+print(d, "is the next day.")
+d.nextDays(7)                  #Call the method in line 68.
+print(d, "is a week later than that.")
+sys.exit(0)
