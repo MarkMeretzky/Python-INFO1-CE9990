4ac3ed797f0614ac2d4de0e33a79563cc14a4250
Mark Meretzky <mark.meretzky@gmail.com>
2017-07-27 16:25:44 -0400

    Rename attributes.py to tabula.py

---
+++ tabula.py
@@ -0,0 +1,40 @@
+"""
+attributes.py
+
+Put some attributes into an initially empty object.
+"""
+
+import sys
+
+class tabulaRasa:
+    pass
+
+
+def myFunc(person):
+    "Print a person's attributes, then change some of them."
+    assert isinstance(person, tabulaRasa)
+
+    if "age" in person.__dict__: #person.__dict__ is a dictionary.
+        del person.age           #Delete an attribute if it's there.
+    if not "hair" in person.__dict__:
+        person.hair = "black"    #Add an attribute if not already there.
+    return person
+
+
+person = tabulaRasa()
+person.name = "Joe"
+person.age = 62
+person.favoriteSongs = ["Help!", "Day Tripper", "Piggies"]
+
+print("Before:")
+for key, value in person.__dict__.items():
+    print(key, value)
+
+myFunc(person)
+print()
+
+print("After:")
+for key, value in person.__dict__.items():
+        print(key, value)
+        
+sys.exit(0)
