df1855c84400f1b55528d60cf743ac13dd5a3a05
Mark Meretzky <mark.meretzky@gmail.com>
2017-07-27 14:43:46 -0400

    Create returntuple.py

---
+++ returntuple.py
@@ -0,0 +1,20 @@
+"""
+returntuple.py
+
+A function that returns a tuple of items.
+"""
+import sys
+
+def stats(myList):
+    "Return the minimum, maximum, and average of the numbers in a list."
+    assert isinstance(myList, list) or isinstance(myList, tuple)
+    assert len(myList) > 0
+    return (min(myList), max(myList), sum(myList) / len(myList))
+
+myList = [10, 20, 50, 30, 20]
+(minimum, maximum, average) = stats(myList)
+
+print("minimum =", minimum)
+print("maximum =", maximum)
+print("average =", average)
+sys.exit(0)
