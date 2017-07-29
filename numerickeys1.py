712f2928f23957f0e34e21714054d5b5b5d662c3
Mark Meretzky <mark.meretzky@gmail.com>
2017-07-29 08:09:38 -0400

    Create numerickeys1.py

---
+++ numerickeys1.py
@@ -0,0 +1,46 @@
+"""
+numerickeys1.py
+
+The keys of this dictionary are consecutive integers.
+A list would be simpler than a dictionary in this case.
+"""
+
+import sys
+
+words = {
+     1: "one",
+     2: "two",
+     3: "three",
+     4: "four",
+     5: "five",
+     6: "six",
+     7: "seven",
+     8: "eight",
+     9: "nine",
+    10: "ten"
+}
+
+while True:
+    try:
+        s = input("Please type an integer from 1 to 10: ")
+    except EOFError:
+        sys.exit(0)
+
+    try:
+        n = int(s)
+    except ValueError:
+        print("Sorry, \"", s, "\" is not an integer.", sep = "")
+        print("Try again.")
+        print()
+        continue   #Go back up to the word "while".
+
+    try:
+        word = words[n]
+    except KeyError:
+        print("Sorry,", s, "is not an integer in the range 1 to 10.")
+        print("Try again.")
+        print()
+        continue   #Go back up to the word "while".
+    
+    print("The word for ", n, " is \"", word, "\".", sep = "")
+    print()
