0385071bf8c12bfe5f32f6cd37855c22fab7c2b8
Mark Meretzky <mark.meretzky@gmail.com>
2017-07-30 11:45:07 -0400

    Create quicksort.py

---
+++ quicksort.py
@@ -0,0 +1,40 @@
+"""
+quicksort.py
+
+A function that sorts a list or tuple.
+"""
+
+import sys
+
+years = [
+    1492,
+    1776,
+    1066,
+    1812,
+    1984,
+    2001,
+     476,
+    2017,
+    1945
+]
+
+
+def quicksort(a):
+    "Return a sorted list of the items in a."
+    assert isinstance(a, list) or isinstance(a, tuple)
+
+    if len(a) == 0:          #could also say if not a:
+        return a
+
+    pivot = a[len(a) // 2]   #the item in the middle
+
+    return \
+          quicksort([item for item in a if item <  pivot]) \
+        +           [item for item in a if item == pivot] \
+        + quicksort([item for item in a if item >  pivot])
+
+
+for year in quicksort(years):
+    print(year)
+
+sys.exit(0)
