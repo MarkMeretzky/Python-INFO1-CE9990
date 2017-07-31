f1a66c702f5f9bbebf5b8f48efaa771e868569f7
Mark Meretzky <mark.meretzky@gmail.com>
2017-07-31 09:51:24 -0400

    Create person.py

---
+++ person.py
@@ -0,0 +1,49 @@
+"""
+person.py
+
+Put some attributes into an initially empty object.
+"""
+
+import sys
+
+class Person:
+    pass
+
+
+def myFunc(person):
+    "Change some of the person's attributes."
+    assert isinstance(person, Person)
+
+    #If person has a hair color, overwrite it.  Otherwise, create it.
+    person.hair = "black"
+
+    #If person has an age, delete it.
+    try:
+        del person.age
+    except AttributeError:  #Arrive here if person had no age.
+        pass                #If person had no age, do nothing.
+
+    try:
+        favoriteSongs = person.favoriteSongs
+    except AttributeError:
+        person.favoriteSongs = [] #Arrive here if person has no favorite songs.
+    person.favoriteSongs.append("Revolution")
+
+
+mark = Person()
+mark.lastName = "Meretzky"
+mark.age = 62
+mark.favoriteSongs = ["Help!", "Day Tripper", "Piggies"]
+
+print("Before:")
+for key, value in mark.__dict__.items():
+    print(key, value)
+
+myFunc(mark)
+print()
+
+print("After:")
+for key, value in mark.__dict__.items():
+    print(key, value)
+        
+sys.exit(0)
