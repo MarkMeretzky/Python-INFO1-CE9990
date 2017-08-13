4a34f2ef0dc293c3f1378212a9225895d2624c04
Mark Meretzky <mark.meretzky@gmail.com>
2017-08-13 12:45:02 -0400

    Create week.py

---
+++ week.py
@@ -0,0 +1,29 @@
+"""
+week.py
+
+A range of 7 consecutive objects of class datetime.date or class
+datetime.datetime, depending on the type of the startingDate.
+"""
+
+import datetime
+import builtins
+
+def range(startingDate):
+    "Yield a range of 8 dates, starting with the startingDate."
+    if not isinstance(startingDate, datetime.date) \
+        and not isinstance(startingDate, datetime.datetime):
+        raise TypeError("startingDate must be datetime.date or "
+            + "datetime.datetime, not " + str(type(startingDate)))
+
+    for i in builtins.range(8):
+        yield startingDate + datetime.timedelta(days = i)
+
+
+if __name__ == "__main__":
+    import sys
+    startingDate = datetime.datetime.now().date()
+    
+    for d in range(startingDate):
+        print(d.strftime("%a %Y-%m-%d"))
+        
+    sys.exit(0)
