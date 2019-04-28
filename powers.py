441cf07558c98e193b8b1b5034fea08c6d62593c
Mark Meretzky <mark.meretzky@gmail.com>
2019-04-28 19:53:06 -0400

    Update powers.py

--- powers.py
+++ powers.py
@@ -1,7 +1,9 @@
 """
 powers.py
 
-Print the powers of 2 less than a million. 
+Print the powers of 2 less than a million.
+One million is a seven-digit number.
+We're printing numbers smaller than a million, so we need only six digits.
 """
 
 import sys
@@ -10,13 +12,11 @@ import math   #for log
 i = 0
 p = 1
 
-while p < 1000000:
-    #One million is a seven-digit number.
-    #We're printing numbers smaller than a million, so we need only six digits.
+while p < 1000000:   #Test at the top of the loop.
     print(f"2 ** {i:2} = {p:6}")
     i += 1   #means i = i + 1
     p *= 2   #means p = p * 2
 
-print()   #skip a line (i.e., output a line consisting only of one newline)
+print()   #Skip a line (i.e., output a line consisting only of one newline).
 print(f"The log to the base 2 of 1000000 is {math.log(1000000, 2)}")
 sys.exit(0)
