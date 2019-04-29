225c622e5c07cf48e72d9006df15a4ff9001c971
Mark Meretzky <mark.meretzky@gmail.com>
2019-04-29 13:59:52 -0400

    Update fontsize.py

--- fontsize.py
+++ fontsize.py
@@ -6,22 +6,24 @@ Output an HTML stylesheet showing different font sizes.
 
 import sys
 
-print("<HTML>")
-print("<BODY>")
-print("<H1>Stylesheet</H1>")
-print()
+print("""\
+<HTML>
+<BODY>
+<H1>Stylesheet</H1>
+""")
 
-points = 1
+for points in range(1, 13):
+    print(f'''\
+<P STYLE = "font-size: {points}pt;">
+{points} point
+<BR>
+Pack my box with five dozen liquor jugs.
+</P>
+''')
 
-while points < 13:
-    print("<P STYLE = \"font-size: ", points, "pt;\">", sep = "")
-    print(points, "point")
-    print("<BR>")
-    print("Pack my box with five dozen liquor jugs.")
-    print("</P>")
-    print()
-    points += 1
+print("""\
+</BODY>
+</HTML>
+""")
 
-print("</BODY>")
-print("</HTML>")
 sys.exit(0)
