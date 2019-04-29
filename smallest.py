a61cf6dc2805da5e20fc751d2c0fb081bcf65ee5
Mark Meretzky <mark.meretzky@gmail.com>
2019-04-29 09:45:09 -0400

    Update smallest.py

--- smallest.py
+++ smallest.py
@@ -13,7 +13,4 @@ while x > 0:
     print(f"{x:23} = {mantissa} * 2 ** {exponent:5}")
     x /= 2   #means x = x / 2, in other words, halve the size of x
 
-print()
-print(f"sys.float_info.min = {sys.float_info.min}")
-print(f"sys.float_info.min_exp = {sys.float_info.min_exp}")
 sys.exit(0)
