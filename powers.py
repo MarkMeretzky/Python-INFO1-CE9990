221352f2b861ed9f0f967a22b2ddd706ea2b1af2
Mark Meretzky <mark.meretzky@gmail.com>
2019-04-29 09:34:59 -0400

    Update powers.py

--- powers.py
+++ powers.py
@@ -18,5 +18,5 @@ while p < 1000000:   #Test at the top of the loop.
     p *= 2   #means p = p * 2
 
 print()   #Skip a line (i.e., output a line consisting only of one newline).
-print(f"The log to the base 2 of 1000000 is {math.log(1000000, 2)}")
+print(f"The log to the base 2 of 1,000,000 is {math.log(1_000_000, 2)}")
 sys.exit(0)
