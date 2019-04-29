28515614e9ddf819e16922bf5998b007e61dbc15
Mark Meretzky <mark.meretzky@gmail.com>
2019-04-29 14:05:16 -0400

    Update nestedloops.py

--- nestedloops.py
+++ nestedloops.py
@@ -6,7 +6,7 @@ Output the chorus of "Lucy in the Sky with Wiamonds".
 
 import sys
 
-for outer in rage(3):   #means range(0, 3)
+for outer in range(3):   #means range(0, 3)
     
     for inner in range(3):
         print("Lucy in the sky with diammonds")
