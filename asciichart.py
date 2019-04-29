9c7567ee18261d7e77033ab66c37b221fb15cad4
Mark Meretzky <mark.meretzky@gmail.com>
2019-04-29 13:33:47 -0400

    Update asciichart.py

--- asciichart.py
+++ asciichart.py
@@ -4,23 +4,19 @@ asciichart.py
 Print the printable ASCII characters (the ones in the range 32 to 126 inclusive)
 and their code numbers in decimal, octal, and hexadecimal.
 
-format string:
-Print the character whose code number is the first argument.
-The second argument will be printed in decimal, left padded with spaces if
-necessary to make it a total of 3 characters.
-The third argument will be printed in octal, left-padded with spaces if
-necessary to make it a total of 3 characters.
-The fourth argument will be printed in uppercase hexadecimal, left-padded with
-zeroes if necessary to make it a total of 2 characters.
+The formatted string:
+
+1. Print the character whose code number is i.
+2. Print i in decimal, left padded with spaces if necessary to make it a total of 3 characters.
+3. Print i in octal, left-padded with spaces if necessary to make it a total of 3 characters.
+4. Print i in uppercase hexadecimal, left-padded with zeroes if necessary to make it a total of 2 characters.
 """
 
 import sys
 
 print("  decimal octal hex")
-i = 32
 
-while i <= 126:
-    print("{:c}     {:3}   {:3o}  {:02X}".format(i, i, i, i))
-    i += 1
+for i in range(32, 127):
+    print(f"{i:c}     {i:3}   {i:3o}  {i:02X}")
 
 sys.exit(0)
