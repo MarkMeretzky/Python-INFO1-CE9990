cb7aae267ff6a3d82d7faebef6f24b992ae6e08c
Mark Meretzky <mark.meretzky@gmail.com>
2019-04-29 13:15:25 -0400

    Update pierogies.py

--- pierogies.py
+++ pierogies.py
@@ -6,10 +6,7 @@ Output the yeild from a recipe.
 
 import sys
 
-h = 2
-
-while h <= 8:
-    print(4 * h, "pierogies feed", 2 * h, "people or", h, "Hungarians.")
-    h += 1   #or h = h + 1
+for h in range(2, 8):
+    print(f"{4 * h} pierogies feed {2 * h} people or {h} Hungarians.")
 
 sys.exit(0)
