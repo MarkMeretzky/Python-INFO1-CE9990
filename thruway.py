58da3bfc4a2e7316835375ae5fc083abeb8e8016
Mark Meretzky <mark.meretzky@gmail.com>
2019-04-29 13:28:40 -0400

    Update thruway.py

--- thruway.py
+++ thruway.py
@@ -1,25 +1,19 @@
 """
 thruway.py
 
-Print the signs going north from New York City on the New York State Thruway. 
+Print the signs going north from New York City on the New York State Thruway.
+The string consists of five lines of text.  The last line is empty.
 """
 
 import sys
 
-#This format string consists of five lines of text.
-
-f = """\
+for a in range(108, 38, -10):
+    print(f"""\
 +--------------+
-| Albany   {:3} |
-| Montreal {:3} |
-| Buffalo  {:3} |
+| Albany   {a:3} |
+| Montreal {a + 220:3} |
+| Buffalo  {a + 280:3} |
 +--------------+
-"""
-
-a = 108   #miles to Albany
-
-while a >= 48:
-    print(f.format(a, a + 220, a + 280))
-    a -= 10   #means a = a - 10
+""")
 
 sys.exit(0)
