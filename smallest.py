206fbd216bc0334abd4d8c670ce43df675334332
Mark Meretzky <mark.meretzky@gmail.com>
2019-04-29 09:46:36 -0400

    Update smallest.py

--- smallest.py
+++ smallest.py
@@ -5,12 +5,10 @@ What is the smallest positive number of type float?
 """
 
 import sys
-import math
 
 x = 1.0
 while x > 0:
-    mantissa, exponent = math.frexp(x)
-    print(f"{x:23} = {mantissa} * 2 ** {exponent:5}")
+    print(f"{x:23}")
     x /= 2   #means x = x / 2, in other words, halve the size of x
 
 sys.exit(0)
