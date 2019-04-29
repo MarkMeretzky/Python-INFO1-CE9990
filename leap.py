e8d77b8197903d989a7bfa3600f19acf4e41a03e
Mark Meretzky <mark.meretzky@gmail.com>
2019-04-28 20:24:40 -0400

    Update leap.py

--- leap.py
+++ leap.py
@@ -11,12 +11,12 @@ year = input("Please type a year: ")
 year = int(year)
 
 if year % 400 == 0:
-    print(year, "is a leap year.")
+    print(f"{year} is a leap year.")
 elif year % 100 == 0:
-    print(year, "is not a leap year.")
+    print(f"{year} is not a leap year.")
 elif year % 4 == 0:
-    print(year, "is a leap year.")
+    print(f"{year} is a leap year.")
 else:
-    print(year, "is not a leap year.")
+    print(f"{year} is not a leap year.")
 
 sys.exit(0)
