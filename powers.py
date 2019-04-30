1f5a5859bc37822120067052394c9f57c75b5c96
Mark Meretzky <mark.meretzky@gmail.com>
2019-04-29 22:08:50 -0400

    Update powers.py

--- powers.py
+++ powers.py
@@ -3,7 +3,7 @@ powers.py
 
 Print the powers of 2 less than a million.
 One million is a seven-digit number.
-We're printing numbers smaller than a million, so we need only six digits.
+We're printing numbers smaller than a million, so we need only 6 digits.
 """
 
 import sys
@@ -12,7 +12,7 @@ import math   #for log
 i = 0
 p = 1
 
-while p < 1000000:   #Test at the top of the loop.
+while p < 1_000_000:   #Test at the top of the loop.
     print(f"2 ** {i:2} = {p:6}")
     i += 1   #means i = i + 1
     p *= 2   #means p = p * 2
