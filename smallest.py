0dc20a2ca3bfc6b5c43f818c2891a978ecdb9abf
Mark Meretzky <mark.meretzky@gmail.com>
2019-04-29 22:10:48 -0400

    Update smallest.py

--- smallest.py
+++ smallest.py
@@ -7,8 +7,11 @@ What is the smallest positive number of type float?
 import sys
 
 x = 1.0
-while x > 0:
+
+while True:
     print(f"{x:23}")
-    x /= 2   #means x = x / 2, in other words, halve the size of x
+    if x == 0:
+        break
+    x /= 2   #means x = x / 2, in other words, halve the value of x
 
 sys.exit(0)
