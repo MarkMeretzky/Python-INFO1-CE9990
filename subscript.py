b5e96592e42e9281e7fa1bbdcfb24eed68880187
Mark Meretzky <mark.meretzky@gmail.com>
2019-05-01 14:59:49 -0400

    Update subscript.py

--- subscript.py
+++ subscript.py
@@ -9,19 +9,19 @@ import sys
 s = "counterclockwise"
 
 #Don't bother to print the period at the end of each sentence.
-print("The value of s is", s)
-print("The type of s is", type(s))
-print("The length of s is", len(s))
+print(f"The value of s is {s}.")
+print(f"The type of s is {type(s)}.")
+print(f"The length of s is {len(s)}.")
 print()
 
-print("The first character is", s[0])
-print("The second character is", s[1])
-print("The third character is", s[2])
-print("The sixteenth character is", s[15])
+print(f"The first character is {s[0]}.")
+print(f"The second character is {s[1]}.")
+print(f"The third character is {s[2]}.")
+print(f"The sixteenth character is {s[15]}.")
 print()
 
-print("The last character is", s[-1])
-print("The next-to-last character is", s[-2])
-print("The third-from-last character is", s[-3])
+print(f"The last character is {s[-1]}.")
+print(f"The next-to-last character is {s[-2]}.")
+print(f"The third-from-last character is {s[-3]}.")
 
 sys.exit(0)
