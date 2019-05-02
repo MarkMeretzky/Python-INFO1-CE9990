32e9ec914287e27b1cafea79a56cb2fb51310187
Mark Meretzky <mark.meretzky@gmail.com>
2019-05-01 22:03:50 -0400

    Update beer.py

--- beer.py
+++ beer.py
@@ -10,7 +10,7 @@ for b in range(100, 0, -1):
     print(f"{b} bottles of beer on the wall,")
     print(f"{b} bottles of beer on the wall--")
     print("If one of those bottles should happen to fall,")
-    print(f"{b} bottles of beer on the wall.")
+    print(f"{b - 1} bottles of beer on the wall.")
 
     print()
     #time.sleep(3)
