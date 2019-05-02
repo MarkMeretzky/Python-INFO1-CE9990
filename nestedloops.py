335fe0393b2f6c149d5a577be8b5ea833841a446
Mark Meretzky <mark.meretzky@gmail.com>
2019-05-01 22:07:18 -0400

    Update nestedloops.py

--- nestedloops.py
+++ nestedloops.py
@@ -11,7 +11,7 @@ for outer in range(3):   #means range(0, 3)
     for inner in range(3):
         print("Lucy in the sky with diammonds")
 
-    print("Aaaaaaaaaaaaaaaaaaaaaaaaaaaaah")
+    print(f'A{28 * "a"}h')
     print()
 
 sys.exit(0)
