db5b3cdd1b3fa339d8d6b96ed9f2825479977147
Mark Meretzky <mark.meretzky@gmail.com>
2019-05-02 10:39:39 -0400

    Update list3.py

--- list3.py
+++ list3.py
@@ -23,15 +23,15 @@ months = [
 
 n = len(months)
 
-print("A year has", n, "months.")
-print(months[0], "is the first month.")
-print(months[1], "is the second month.")
-print(months[2], "is the third month.")
-print(months[3], "is the cruelest month.")
-print(months[11], "is the twelfth month.")
+print(f"A year has {n} months.")
+print(f"{months[0]} is the first month.")
+print(f"{months[1]} is the second month.")
+print(f"{months[2]} is the third month.")
+print(f"{months[3]} is the cruelest month.")
+print(f"{months[11]} is the twelfth month.")
 
-print(months[-1], "is the last month.")
-print(months[-2], "is the next-to-last month.")
-print(months[-3], "is the third-to-last month.")
+print(f"{months[-1]} is the last month.")
+print(f"{months[-2]} is the next-to-last month.")
+print(f"{months[-3]} is the third-to-last month.")
 
 sys.exit(0)
