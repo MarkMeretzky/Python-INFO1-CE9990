6cc9b0a5cd9a714c752d317bf30af4f819268ee3
Mark Meretzky <mark.meretzky@gmail.com>
2019-05-06 16:07:00 -0400

    Update apollo.py

--- apollo.py
+++ apollo.py
@@ -1,24 +1,17 @@
 """
 apollo.py
 
-Demonstrate three ways to loop through the characters of a string.
+Demonstrate two ways to loop through the characters of a string.
 """
 
 import sys
 
 s = "APOLLO"
 
-i = 0
-while i < len(s):
-    print(s[i])
-    i += 1
-
-print()   #Skip a line (i.e., output a line consisting of one newline character).
-
-for i in range(0, len(s)):   #Don't need to write the 0,
+for i in range(len(s)):
     print(s[i])
 
-print()
+print()   #Skip a line, i.e., output a line consisting of one newline character.
 
 for c in s:
     print(c)
