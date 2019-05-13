b61c80700dcf03a10031fdd6d3dcfe802df556e7
Mark Meretzky <mark.meretzky@gmail.com>
2019-05-13 14:52:58 -0400

    Update heterogeneous.py

--- heterogeneous.py
+++ heterogeneous.py
@@ -7,19 +7,27 @@ are of four different data types.
 
 import sys
 
+#movie[0] is a string.
+#movie[1] is an int.
+#movie[2] is a float.
+#movie[3] is a list of strings.
+
 movie = ["Dr. No", 1962, 59_500_000.00, ["Sean Connery", "Ursula Andress", "Joseph Wiseman", "Jack Lord"]]
 
-print("Name:", movie[0])                          #movie[0] is a string.
-print("Year:", movie[1])                          #movie[1] is an int.
-print("Box office:", "${:,.2f}".format(movie[2])) #movie[2] is a float.
-print("Starring: ", end = "")                     #movie[3] is a list of strings.
+
+print("""\
+Name: {movie[0]}                          
+Year: {movie[1]}                         
+Box office: ${movie[2]:,.2f} 
+Starring:""",
+    end = "")                     
 
 stars = movie[3]
 
 for i, star in enumerate(stars):
     if i == len(stars) - 1:
-        print(star)             #the last star
+        print(star)                  #the last star
     else:
-        print(star, end = ", ") #the other stars
+        print(f"{star}, ", end = "") #the other stars
 
 sys.exit(0)
