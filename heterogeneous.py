12b588b6017bcf0469f7d520132d0cf2fb8a72ef
Mark Meretzky <mark.meretzky@gmail.com>
2019-05-13 14:59:30 -0400

    Update heterogeneous.py

--- heterogeneous.py
+++ heterogeneous.py
@@ -27,6 +27,6 @@ stars = movie[3]
 for star in stars[:-1]: #all but the last star
     print(f"{star}, ", end = "")
   
-print(star[:-1])         #the last star
+print(stars[:-1])       #the last star
 
 sys.exit(0)
