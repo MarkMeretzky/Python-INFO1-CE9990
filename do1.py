0ce1174fd275afc84127dbace860fca75b2b37e4
Mark Meretzky <mark.meretzky@gmail.com>
2019-05-16 15:36:38 -0400

    Update do1.py

--- do1.py
+++ do1.py
@@ -25,11 +25,11 @@ while True:
 
     for note in notes:   #note is a list of two strings
         if note[0] == n:
-            print(n.capitalize(), ", a ", note[1], ".", sep = "")
+            print(f"{n.capitalize()}, a {note[1]}.")
             break        #Break out of the for loop, skipping the else.
     else:
         #Arrive here if the break was not executed,
         #i.e., if the for loop did not find n.
-        print("Sorry, \"", n, "\" is not a note.", sep = "")
+        print(f"Sorry, \"{n}\" is not a note.")
         
     print()
