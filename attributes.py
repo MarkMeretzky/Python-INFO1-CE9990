64aebdcc518ecd28b10adb610ed6780068890a8c
Mark Meretzky <mark.meretzky@gmail.com>
2019-05-20 23:26:29 -0400

    Update attributes.py

--- attributes.py
+++ attributes.py
@@ -8,10 +8,10 @@ import sys
 attributes = {
     "name":           "Mark",            #string
     "zipcode":        10705,             #int
-    "age":            61.9,              #float
+    "age":            63.75,             #float
     "citizenship":    True,              #bool
     "leaning":        "flaming liberal", #string
-    "favorite years": [1968, 2001, 2017] #list
+    "favorite years": [1968, 2001, 2019] #list
 }
 
 while True:
