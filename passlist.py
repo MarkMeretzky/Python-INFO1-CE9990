aacdfc2d819d688c2e1cb16dab3bcb5a7b35b434
Mark Meretzky <mark.meretzky@gmail.com>
2019-05-22 16:24:25 -0400

    Update passlist.py

--- passlist.py
+++ passlist.py
@@ -9,13 +9,13 @@ argument (line 25).
 import sys
 
 def threeArgs(arg0, arg1, arg2):
-    print("threeArgs:", arg0, arg1, arg2)
+    print(f"threeArgs: {arg0} {arg1} {arg2}")
 
 def oneArg(arg0):
     assert isinstance(arg0, list) or isinstance(arg0, tuple)
     print("oneArg:", end = "")
     for arg in arg0:
-        print(" ", arg, sep = "", end = "")
+        print(f" {arg}, end = "")
     print()
 
 threeArgs("Moe", "Larry", "Curly")
