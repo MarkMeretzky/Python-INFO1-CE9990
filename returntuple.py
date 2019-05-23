50655b5d4740396115f4472881da569f9fc74f54
Mark Meretzky <mark.meretzky@gmail.com>
2019-05-23 15:15:58 -0400

    Update returntuple.py

--- returntuple.py
+++ returntuple.py
@@ -9,10 +9,10 @@ def stats(myList):
     "Return the minimum, maximum, and average of the numbers in a list."
     assert isinstance(myList, list) or isinstance(myList, tuple)
     assert len(myList) > 0   #Don't divide by zero in the next statement.
-    return (min(myList), max(myList), sum(myList) / len(myList))
+    return (min(myList), max(myList), sum(myList) / len(myList)) #outer parens unnecessary
 
 myList = [10, 20, 50, 30, 20]
-(minimum, maximum, average) = stats(myList)
+(minimum, maximum, average) = stats(myList) #parens around variables unnecessary
 
 print("minimum =", minimum)
 print("maximum =", maximum)
