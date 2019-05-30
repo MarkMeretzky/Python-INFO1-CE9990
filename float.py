90e90513ca3cd4f1631e4f3132c83fd8638e0ae3
Mark Meretzky <mark.meretzky@gmail.com>
2019-05-30 15:47:48 -0400

    Update float.py

--- float.py
+++ float.py
@@ -10,16 +10,16 @@ def range(start, end, n):
     "Print n+1 equally spaced numbers, from start to end inclusive."
 
     if not isinstance(start, float) and not isinstance(start, int):
-        raise TypeError("start must be float or int, not " + str(type(start)))
+        raise TypeError(f"start must be float or int, not {type(start)}")
         
     if not isinstance(end, float) and not isinstance(end, int):
-        raise TypeError("end must be float or int, not " + str(type(end)))
+        raise TypeError(f"end must be float or int, not {type(end)}")
 
     if not isinstance(n, int):
-        raise TypeError("n must be int, not " + str(type(n)))
+        raise TypeError(f"n must be int, not {type(n)}")
 
     if n <= 0:
-        raise ValueError("n must be positive, not " + str(n))
+        raise ValueError(f"n must be positive, not {n}")
 
     for i in builtins.range(n + 1):   #the built-in range
         yield start + (end - start) * i / n
