6787270d432bd1f97802c5bec37baa536c1d78b2
Mark Meretzky <mark.meretzky@gmail.com>
2019-05-30 15:51:23 -0400

    Update floatdemo.py

--- floatdemo.py
+++ floatdemo.py
@@ -12,14 +12,22 @@ for i in range(10 + 1):
     print(i)
 print()
 
-#The range we wrote: print 11 equally spaced numbers from 0.0 to 10.0 inclusive.
-for f in float.range(0.0, 1.0, 10):
-    print(f)
-print()
+try:
+    #The range we wrote: print 11 equally spaced numbers from 0.0 to 10.0 inclusive.
+    for f in float.range(0.0, 1.0, 10):
+        print(f)
+    print()
 
-if .7 in float.range(0.0, 1.0, 10):
-    print(".7 is in the float.range.")
-else:
-    print(".7 is not in the float.range.")
+    if .7 in float.range(0.0, 1.0, 10):
+        print(".7 is in the float.range.")
+    else:
+        print(".7 is not in the float.range.")
+
+except TypeError as error:
+    print(error)
+    sys.exit(1)
+except ValueError as error:
+    print(error)
+    sys.exit(1)
 
 sys.exit(0)
