5342f58de8a5c1c2090a3f0d98b2171ad9085527
Mark Meretzky <mark.meretzky@gmail.com>
2019-08-16 11:25:15 -0400

    Update knockknock.py

--- knockknock.py
+++ knockknock.py
@@ -10,15 +10,21 @@ try:
     kk = input("Tell me a knock-knock joke. ")
 except EOFError:
     sys.exit(1)
+except KeyboardInterrupt:
+    sys.exit(1)
 
 try:
     name = input("Who's there? ")
 except EOFError:
     sys.exit(1)
+except KeyboardInterrupt:
+    sys.exit(1)
 
 try:
     punchline = input(f"{name} who? ")
 except EOFError:
+    sys.exit(1)
+except KeyboardInterrupt:
     sys.exit(1)
 
 print(f"Ha{5 * 'ha'}!")
