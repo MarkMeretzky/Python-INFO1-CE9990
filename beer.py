2f44097dcf66d63d6ccbd860f4ba5583eac9d73c
Mark Meretzky <mark.meretzky@gmail.com>
2019-08-22 14:47:37 -0400

    Update beer.py

--- beer.py
+++ beer.py
@@ -5,13 +5,13 @@ Output the lyrics to "A Hundred Bottles of Beer on the Wall.
 """
 
 import sys
+#import time
 
 for b in range(100, 0, -1):
     print(f"{b} bottles of beer on the wall,")
     print(f"{b} bottles of beer on the wall--")
     print("If one of those bottles should happen to fall,")
     print(f"{b - 1} bottles of beer on the wall.")
-
     print()
     #time.sleep(3)
 
