4a08e3d5ec1242fcfaa569cd79877d159606f3c2
Mark Meretzky <mark.meretzky@gmail.com>
2019-08-22 14:41:45 -0400

    Update asciichart.py

--- asciichart.py
+++ asciichart.py
@@ -7,9 +7,9 @@ and their code numbers in decimal, octal, and hexadecimal.
 The formatted string:
 
 1. Print the character whose code number is i.
-2. Print i in decimal, left padded with spaces if necessary to make it a total of 3 characters.
-3. Print i in octal, left-padded with spaces if necessary to make it a total of 3 characters.
-4. Print i in uppercase hexadecimal, left-padded with zeroes if necessary to make it a total of 2 characters.
+2. Print i in decimal, left padded with spaces if necessary to make it a total of 7 characters.
+3. Print i in octal, left-padded with spaces if necessary to make it a total of 5 characters.
+4. Print i in uppercase hexadecimal, left-padded with zeroes if necessary to make it a total of 3 characters.
 """
 
 import sys
@@ -17,6 +17,6 @@ import sys
 print("  decimal octal hex")
 
 for i in range(32, 127):
-    print(f"{i:c}     {i:3}   {i:3o}  {i:02X}")
+    print(f"{i:c} {i:7} {i:5o} {i:03X}")
 
 sys.exit(0)
