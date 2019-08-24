84a638e62d47956ce2e37c7443890cf0a25fc2cc
Mark Meretzky <mark.meretzky@gmail.com>
2019-08-24 10:05:46 -0400

    Update filereader.py

--- filereader.py
+++ filereader.py
@@ -14,11 +14,11 @@ filename = "/Library/Frameworks/Python.framework/Versions/3.7/share/doc/python3.
 
 try:
     lines = open(filename) #lines is something you can loop through with a for loop
-except FileNotFoundError:
-    print(f"Sorry, could not find file \"{filename}\".")
+except FileNotFoundError as error:
+    print(error, file = sys.stderr)
     sys.exit(1)
-except PermissionError:
-    print(f"Sorry, no permission to open file \"{filename}\".")
+except PermissionError as error:
+    print(error, file = sys.stderr)
     sys.exit(1)
 
 for line in lines:
