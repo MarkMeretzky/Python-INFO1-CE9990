6d0bb88f7404793c388c9cf03d74ce83b4ccddbf
Mark Meretzky <mark.meretzky@gmail.com>
2019-08-25 09:12:46 -0400

    Update asciichart.py

--- asciichart.py
+++ asciichart.py
@@ -2,15 +2,15 @@
 asciichart.py
 
 Print the printable ASCII characters (the ones in the range 32 to 126 inclusive)
-and their code numbers in decimal, octal, and hexadecimal.
+and their code numbers in decimal (base 10), octal (base 8), hexadecimal (base 16), and binary (base 2).
 
 The formatted string:
 
 1. Print the character whose code number is i.
-2. Print i in decimal, left padded with spaces if necessary to make it a minimum of 7 characters.
-3. Print i in octal, left-padded with spaces if necessary to make it a minimum of 5 characters.
-4. Print i in uppercase hexadecimal, left-padded with zeroes if necessary to make it a minimum of 2 characters.
-5. Prit i in binary, left-padded with zeroes if necessary to make it a minimum of 8 characters
+2. Print i in decimal, left padded with spaces if necessary to use a minimum of 7 characters.
+3. Print i in octal, left-padded with spaces if necessary to use a minimum of 5 characters.
+4. Print i in uppercase hexadecimal, left-padded with zeroes if necessary to use a minimum of 2 characters.
+5. Prit i in binary, left-padded with zeroes if necessary to use a minimum of 8 characters
 """
 
 import sys
