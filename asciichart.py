c39f548bbdf40cf5af2a8eddb52727fb3832aa6e
Mark Meretzky <mark.meretzky@gmail.com>
2019-09-03 09:27:10 -0400

    Update asciichart.py

--- asciichart.py
+++ asciichart.py
@@ -7,17 +7,18 @@ and their code numbers in decimal (base 10), octal (base 8), hexadecimal (base 1
 The formatted string:
 
 1. Print the character whose code number is i.
-2. Print i in decimal, left padded with spaces if necessary to use a minimum of 7 characters.
-3. Print i in octal, left-padded with spaces if necessary to use a minimum of 5 characters.
+2. Print i in decimal, left-padded with spaces if necessary to use a minimum of 9 characters.
+3. Print i in octal, left-padded with spaces if necessary to use a minimum of 8 characters.
 4. Print i in uppercase hexadecimal, left-padded with zeroes if necessary to use a minimum of 2 characters.
 5. Prit i in binary, left-padded with zeroes if necessary to use a minimum of 8 characters
 """
 
 import sys
 
-print("  decimal octal hex   binary")
+print("    decimal    octal       hex   binary")
+print("  (base 10) (base 8) (base 16) (base 2)")
 
 for i in range(32, 127):
-    print(f"{i:c} {i:7} {i:5o}  {i:02X} {i:08b}")
+    print(f"{i:c} {i:9} {i:8o}        {i:02X} {i:08b}")
 
 sys.exit(0)
