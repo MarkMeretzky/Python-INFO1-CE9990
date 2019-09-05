c5728de83dddad09951386466dc909a8775f33b6
Mark Meretzky <mark.meretzky@gmail.com>
2019-09-05 13:28:04 -0400

    Create onebigsequenceofbytes.py

---
+++ onebigsequenceofbytes.py
@@ -0,0 +1,29 @@
+"""
+onebigsequenceofbytes.py
+
+Read an entire text file into one big sequence of bytes.
+Convert the sequence of bytes to a string of characters, and print it.
+"""
+
+import sys
+import urllib.request
+
+url = "http://oit2.scps.nyu.edu/~meretzkm/python/string/romeo.txt"
+
+try:
+    fileObject = urllib.request.urlopen(url)
+except urllib.error.URLError as error:
+    print(error, file = sys.stderr)
+    sys.exit(1)
+
+sequenceOfBytes = fileObject.read()
+fileObject.close()
+
+try:
+    s = sequenceOfBytes.decode("utf-8") #Convert sequence of bytes to string of characters.
+except UnicodeError as error:
+    print(error, file = sys.stderr)
+    sys.exit(1)
+
+print(s, end = "")
+sys.exit(0)
