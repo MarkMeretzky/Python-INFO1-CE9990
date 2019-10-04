2c350eb83a46342890da2611f0209d63f1412db4
Mark Meretzky <mark.meretzky@gmail.com>
2019-10-04 09:11:21 -0400

    Update elementtree.py

--- elementtree.py
+++ elementtree.py
@@ -6,22 +6,25 @@ without using third-party software.
 """
 
 import sys
+import urllib.parse
 import urllib.request
 import xml.etree.ElementTree
 import xml.dom.minidom
 
-url = (
-    "http://api.openweathermap.org/data/2.5/weather"
-    "?q=10004,US"
-    "&units=imperial"
-    "&mode=xml"
-    "&APPID=532d313d6a9ec4ea93eb89696983e369"
-)
+query = {                              #query is a dictionary
+    "q":     "10004,US",
+    "units": "imperial",
+    "mode":  "xml",
+    "APPID": "532d313d6a9ec4ea93eb89696983e369"
+}
+
+params = urllib.parse.urlencode(query) #params is a string
+url = f"http://api.openweathermap.org/data/2.5/weather?{params}"
 
 try:
     infile = urllib.request.urlopen(url)
 except urllib.error.URLError as error:
-    print("urllib.error.URLError", error)
+    print(error, file = sys.stderr)
     sys.exit(1)
 
 sequenceOfBytes = infile.read()         #Read the entire input file.
@@ -29,8 +32,8 @@ infile.close()
 
 try:
     s = sequenceOfBytes.decode("utf-8") #s is a string.
-except UnicodeError as unicodeError:
-    print("UnicodeError", unicodeError)
+except UnicodeError as error:
+    print(error, file = sys.stderr)
     sys.exit(1)
 
 print(s)
@@ -41,7 +44,7 @@ print()
 try:
     document = xml.dom.minidom.parseString(s)
 except xml.parsers.expat.ExpatError as error:
-    print("xml.parsers.expat.ExpatError", error)
+    print(error, file = sys.stderr)
     sys.exit(1)
 
 prettyS = document.toprettyxml(indent = "\t")   #prettyS is a string.
@@ -53,22 +56,22 @@ print()
 try:
     root = xml.etree.ElementTree.fromstring(s)
 except xml.etree.ElementTree.ParseError as error:
-    print("xml.etree.ElementTree.ParseError", error)
+    print(error, file = sys.stderr)
     sys.exit(1)
 
 temperature = root.find("temperature")
 if temperature == None:
-    print("Couldn't find temperature.")
+    print("Couldn't find temperature.", file = sys.stderr)
     sys.exit(1)
 
 value = temperature.get("value")
 if value == None:
-    print("Couldn't find value of temperature.")
+    print("Couldn't find value of temperature.", file = sys.stderr)
     sys.exit(1)
 
 unit = temperature.get("unit")
 if unit == None:
-    print("Couldn't find unit of temperature.")
+    print("Couldn't find unit of temperature.", file = sys.stderr)
     sys.exit(1)
 
 print(f"The temperature is {value}° {unit}.")
