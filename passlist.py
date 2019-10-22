9bc0e661dc66ebc6459b27119091c86d70d6f8a6
Mark Meretzky <mark.meretzky@gmail.com>
2019-10-22 15:02:20 -0400

    Update passlist.py

--- passlist.py
+++ passlist.py
@@ -1,9 +1,9 @@
 """
 passlist.py
 
-Pass a list or tuple to a function that expects one argument (line 22),
+Pass a list or tuple to a function that expects one argument (line 21),
 and to a function that expects to receive each item on the list as a separate
-argument (line 23).
+argument (line 22).
 """
 
 import sys
@@ -13,8 +13,7 @@ def threeArgs(arg0, arg1, arg2):
 
 def oneArg(arg0):
     assert isinstance(arg0, list) or isinstance(arg0, tuple)
-    print(" ".join(arg0))
-    print()
+    print(f'oneArg: {" ".join(arg0)}')
 
 threeArgs("Moe", "Larry", "Curly")
 
