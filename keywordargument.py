bfebafbb7b23cdeeec851a66a1cfc54b8d3b8aa1
Mark Meretzky <mark.meretzky@gmail.com>
2019-10-22 15:12:12 -0400

    Update keywordargument.py

--- keywordargument.py
+++ keywordargument.py
@@ -12,7 +12,7 @@ def printLocation(latitude, longitude, altitude = 0):
     "Print the arguments.  The third one defaults to 0."
     print(latitude, longitude, altitude)
 
-printLocation(40, -75)
-printLocation(40, -75, 1000)
-printLocation(40, -75, altitude = 1000)
+printLocation(40, -74)
+printLocation(40, -74, 1000)
+printLocation(40, -74, altitude = 1000)
 sys.exit(0)
