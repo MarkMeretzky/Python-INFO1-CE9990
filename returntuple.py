7836e2d23e0d28cf7411043d107048d328798b34
Mark Meretzky <mark.meretzky@gmail.com>
2019-10-25 17:56:15 -0400

    Update returntuple.py

--- returntuple.py
+++ returntuple.py
@@ -5,14 +5,23 @@ A function that returns a tuple of items.
 """
 import sys
 
-def stats(myList):
-    "Return the minimum, maximum, and average of the numbers in a list."
-    assert isinstance(myList, list) or isinstance(myList, tuple)
-    assert len(myList) > 0   #Don't divide by zero in the next statement.
-    return (min(myList), max(myList), sum(myList) / len(myList)) #outer parens unnecessary
+def stats(collection):
+    "Return the minimum, maximum, and average value in the collection."
+    try:
+        _ = iter(collection)
+    except TypeError as error:
+        print(f"Sorry, an argument of type {type(arg0)} is not iterable.", file = sys.stderr)
+        sys.exit(1)
 
-myList = [10, 20, 50, 30, 20]
-(minimum, maximum, average) = stats(myList) #parens around the three variables are unnecessary
+    assert len(collection) > 0 #Don't let line 20 divide by 0.
+    listOfBools = [isinstance(item, (int, float)) for item in collection]
+    assert all(listOfBools)    #Assert that all the bools are True.
+
+    return (min(collection), max(collection), sum(collection) / len(collection)) #outer parens unnecessary
+
+
+listOfInts = [10, 20, 50, 30, 20]
+(minimum, maximum, average) = stats(listOfInts) #parens around the three variables are unnecessary
 
 print(f"minimum = {minimum}")
 print(f"maximum = {maximum}")
