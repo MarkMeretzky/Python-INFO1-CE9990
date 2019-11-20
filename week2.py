b4ac87910284c89517faec44dcbe9db01944c677
Mark Meretzky <mark.meretzky@gmail.com>
2019-11-19 22:28:49 -0500

    Update week2.py

--- week2.py
+++ week2.py
@@ -1,7 +1,7 @@
 """
 week2.py
 
-Class range is an interable for iterating through 7 consecutive datetime.date
+Class range is an interable for iterating through 8 consecutive datetime.date
 or datetime.datetime objects, depending on the type of the startingDate passed
 to the __init__ method.
 """
@@ -9,21 +9,20 @@ to the __init__ method.
 import datetime
 
 class range(object):
-    "An iterable for iterating through 7 consecutive datetime.datetime objects."
+    "An iterable for iterating through 8 consecutive datetime.datetime objects."
 
     def __init__(self, startingDate):
         assert isinstance(startingDate, datetime.date)
         self.startingDate = startingDate
 
     def __iter__(self):
-        return Iterator(self)
+        return Iterator(self.startingDate)
 
 
 class Iterator(object):
 
-    def __init__(self, r):
-        assert isinstance(r, range)
-        self.r = r
+    def __init__(self, startingDate):
+        self.startingDate = startingDate
         self.i = 0
 
     def __iter__(self):
@@ -32,14 +31,14 @@ class Iterator(object):
     def __next__(self):
         if self.i >= 7:
             raise StopIteration
-        result = self.r.startingDate + datetime.timedelta(days = self.i)
+        result = self.startingDate + datetime.timedelta(days = self.i)
         self.i += 1
         return result
 
 
 if __name__ == "__main__":
     import sys
-    startingDate = datetime.datetime.now().date()
+    startingDate = datetime.datetime.today()
     for d in range(startingDate):
         print(d.strftime("%a %Y-%m-%d"))
     sys.exit(0)
