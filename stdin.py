9b9a66758a432f9ca95e3002cad8ea117a105fde
Mark Meretzky <mark.meretzky@gmail.com>
2020-02-11 05:37:35 -0500

    Update stdin.py

--- stdin.py
+++ stdin.py
@@ -1,14 +1,14 @@
 """
 stdin.py
 
-Prompt the user for their name.
+Prompt the user for his or her name.
 Read the name from the standard input, and send a geeting to the standard
 output. 
 """
 
 import sys
 
-first = input("What is your first name? ")
+first = input("What is your first name? ")   #final space for legibility
 last = input("What is your last name? ")
 print(f"Hello, {first} {last}.")
 
